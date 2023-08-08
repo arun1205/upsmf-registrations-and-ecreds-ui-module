36c1bff3c0e016163bfbabc0a41c783e169fa8c6
Arun Kumar <arun.kumar@tarento.com>
2023-08-08 14:05:11 +0530

    common snackbar implemented !!

--- upsmf-registration-and-ecreds-web-module/src/app/services/base-interceptor.interceptor.ts
+++ upsmf-registration-and-ecreds-web-module/src/app/services/base-interceptor.interceptor.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
+  HttpResponse,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { Observable,throwError } from 'rxjs';
+import { catchError, tap, } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable()
 export class BaseInterceptorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private snackBar: MatSnackBar) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      tap(e => {
+        console.log(e)
+        if (request.method == "POST" || request.method == "PUT")
+          if (e instanceof HttpResponse && e.status == 200) {
+            this.snackBar.open('Action completed successfully !!', 'close', {
+              duration:5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'end',
+              panelClass: 'red-snackbar',
+            });
+          }
+      }),
+      catchError(error => {
+        this.snackBar.open('Error while completing action.', 'close', {
+          duration:5000,
+          verticalPosition: 'top',
+          horizontalPosition: 'end',
+          panelClass: 'red-snackbar',
+        });
+        return throwError(error);
+      })
+    );
+   // return next.handle(request);
   }
 }
+
+
